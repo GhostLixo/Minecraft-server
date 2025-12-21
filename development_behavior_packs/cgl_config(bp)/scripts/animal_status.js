0372f6f20fcf8437284e82a5524e3801880708ee
Arnimisal <arnimisal412@gmail.com>
2025-12-21 15:24:55 -0300

    Update animal_status.js

--- development_behavior_packs/cgl_config(bp)/scripts/animal_status.js
+++ development_behavior_packs/cgl_config(bp)/scripts/animal_status.js
@@ -5,7 +5,7 @@ system.runInterval(() =>{
     for (const animal of world.getDimension("overworld").getEntities()){
         const vida_score = animal.getComponent("health")
         if(vida_score) {
-            animal.nameTag = ` ${vida_score.currentValue}`
+            animal.nameTag = ` ${vida_score.currentValue.toFixed(2)}`
         }
     }
 })
