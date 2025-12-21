840ecda52fa08b292b728a3b44fbc59bdbe951db
Arnimisal <arnimisal412@gmail.com>
2025-12-21 20:42:39 -0300

    Update abilidades.js

--- development_behavior_packs/cgl_config(bp)/scripts/abilidades.js
+++ development_behavior_packs/cgl_config(bp)/scripts/abilidades.js
@@ -1,13 +1,15 @@
- import {world,  system} from "@minecraft/server"
+ import {world,  system, InputButton, ButtonState} from "@minecraft/server"
 
  system.runInterval(()=>{
-     const dash_scoreboard = world.scoreboard.getObjective("dash")
-     const dash_max_scoreboard = world.scoreboard.getObjective("dash_max")
-    for (const player of world.getAllPlayers()) {
+    world.afterEvents.playerButtonInput.subscribe((ev) =>{
+        const {player, button, newButtonState} = ev
+        const dash_scoreboard = world.scoreboard.getObjective("dash")
+        const dash_max_scoreboard = world.scoreboard.getObjective("dash_max")
         const dash_score = dash_scoreboard.getScore(player) ?? 0
         const dash_max_score = dash_max_scoreboard.getScore(player) ?? 0
-        if(!player.isOnGround) {
-            if(player.isJumping && dash_score > 0) {
+        if (button === InputButton.Jump && newButtonState === ButtonState.Released) {
+            if(!player.isOnGround) {
+            if(dash_score > 0) {
                 const ViewDirection = player.getViewDirection()
                 player.applyKnockback(ViewDirection, ViewDirection.y)
                 dash_scoreboard.addScore(player, -1)
@@ -15,6 +17,6 @@
         } else {
             dash_scoreboard.setScore(player, dash_max_score)
         }
-    }
- 
+        }
+    })  
  });
