cf65b65297d46679de68335b64bd77f1ba2f4848
Arnimisal <arnimisal412@gmail.com>
2025-12-21 14:05:59 -0300

    Update status.js

--- development_behavior_packs/cgl_config(bp)/scripts/status.js
+++ development_behavior_packs/cgl_config(bp)/scripts/status.js
@@ -2,7 +2,8 @@ import { world, system } from "@minecraft/server"
 
 system.runInterval(() =>{
     const mana_scoreboard = world.scoreboard.getObjective("mana")
-
+    console.warn(mana_scoreboard);
+    
     for (const jogador of world.getAllPlayers()) {
         const manaScore = mana_scoreboard.getScore(jogador);
         jogador.onScreenDisplay.setTitle(`teste123`)
