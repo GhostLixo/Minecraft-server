c07010686d33f158eb8ad62df4a1671e4841aafc
Arnimisal <arnimisal412@gmail.com>
2025-12-21 17:43:58 -0300

    Update saudacao.js

--- development_behavior_packs/cgl_config(bp)/scripts/saudacao.js
+++ development_behavior_packs/cgl_config(bp)/scripts/saudacao.js
@@ -2,13 +2,17 @@ import { world, system, TicksPerSecond } from "@minecraft/server";
 
 world.afterEvents.playerJoin.subscribe((ev) => {
     system.runTimeout(() => {
+      const mana_scoreboard = world.scoreboard.getObjective("mana")
+      const estamina_scoreboard = world.scoreboard.getObjective("estamina")
 
       const jugador = ev.playerId;
       for (let keyl of world.getAllPlayers()) {
         if (keyl.id === jugador) {
-          keyl.onScreenDisplay.setTitle("§l§aSeja bem-vindo!" +  ev.playerName);
-          world.sendMessage("Bem vindo " + ev.playerName);
-          
+          estamina_scoreboard.setScore(keyl, 0)
+          mana_scoreboard.setScore(keyl, 0)
+          keyl.onScreenDisplay.setTitle("§l§aSeja bem-vindo!\n" +  ev.playerName);
+          world.sendMessage("Bem vindo " + ev.playerId);
+          world.sendMessage("Bem vindxo " + ev.playerName);
             }
         }
 
