84d0bcb303cae5dc3acdd2ff50c728746cfa0953
Arnimisal <arnimisal412@gmail.com>
2025-12-21 14:51:31 -0300

    Update animal_status.js

--- development_behavior_packs/cgl_config(bp)/scripts/animal_status.js
+++ development_behavior_packs/cgl_config(bp)/scripts/animal_status.js
@@ -3,7 +3,9 @@ import { world, system } from "@minecraft/server"
 system.runInterval(() =>{
 
     for (const animal of world.getDimension("overworld").getEntities()){
-        const vida_score = animal.getComponent("health") 
-        animal.nameTag(`vida: ${vida_score.currentValue}`)
+        const vida_score = animal.getComponent("health")
+        if(vida_score) {
+            animal.nameTag(`vida: ${vida_score.currentValue}`)
+        }
     }
 })
