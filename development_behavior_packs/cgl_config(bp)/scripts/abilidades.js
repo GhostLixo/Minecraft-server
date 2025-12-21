c72eebbf692fe7d230664c64c31727b85262d336
Arnimisal <arnimisal412@gmail.com>
2025-12-21 20:56:20 -0300

    Update abilidades.js

--- development_behavior_packs/cgl_config(bp)/scripts/abilidades.js
+++ development_behavior_packs/cgl_config(bp)/scripts/abilidades.js
@@ -1,12 +1,12 @@
  import {world,  system, InputButton, ButtonState} from "@minecraft/server"
 
  system.runInterval(()=>{
-    world.afterEvents.playerButtonInput.subscribe((ev) =>{
-        const {player, button, newButtonState} = ev
-        const dash_scoreboard = world.scoreboard.getObjective("dash")
+    const dash_scoreboard = world.scoreboard.getObjective("dash")
         const dash_max_scoreboard = world.scoreboard.getObjective("dash_max")
         const dash_score = dash_scoreboard.getScore(player) ?? 0
         const dash_max_score = dash_max_scoreboard.getScore(player) ?? 0
+    world.afterEvents.playerButtonInput.subscribe((ev) =>{
+        const {player, button, newButtonState} = ev
         if (button === InputButton.Jump && newButtonState === ButtonState.Released) {
             if(!player.isOnGround) {
             if(dash_score > 0) {
