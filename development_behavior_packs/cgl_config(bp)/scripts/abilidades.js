d4f5bad9bc41dc173a9c5935374670dcdc8558aa
Arnimisal <arnimisal412@gmail.com>
2025-12-21 22:40:02 -0300

    Update abilidades.js

--- development_behavior_packs/cgl_config(bp)/scripts/abilidades.js
+++ development_behavior_packs/cgl_config(bp)/scripts/abilidades.js
@@ -1,13 +1,8 @@
  import {world,  system, InputButton, ButtonState} from "@minecraft/server"
- // double_jump_scoreboard do player = 2]
-system.run(()=>{
-const dash_max_scoreboard = world.scoreboard.getObjective("dash_max")
-const double_jump_scoreboard = world.scoreboard.getObjective("double_jump")
-const dash_scoreboard = world.scoreboard.getObjective("dash")
-})
-
+ // double_jump_scoreboard do player = 2
 
 world.afterEvents.playerButtonInput.subscribe((ev) =>{
+    const double_jump_scoreboard = world.scoreboard.getObjective("double_jump")
 
     const {player, button, newButtonState} = ev
 
@@ -29,6 +24,9 @@ world.afterEvents.playerButtonInput.subscribe((ev) =>{
     })
 
 system.runInterval(()=>{
+    const dash_max_scoreboard = world.scoreboard.getObjective("dash_max")
+    const double_jump_scoreboard = world.scoreboard.getObjective("double_jump")
+    const dash_scoreboard = world.scoreboard.getObjective("dash")
 
     for (const player of world.getAllPlayers()) {
         const dash_max_score = dash_max_scoreboard.getScore(player) ?? 0
