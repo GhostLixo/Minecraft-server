889dc7887fcdde04545aa0b52ebad1a323bd36e3
Ghost Lixo <martinsclaudiojunior@gmail.com>
2025-12-22 12:15:22 -0300

    nada

--- development_behavior_packs/cgl_config(bp)/scripts/abilidades.js
+++ development_behavior_packs/cgl_config(bp)/scripts/abilidades.js
@@ -1,17 +1,14 @@
- import {world,  system, InputButton, ButtonState} from "@minecraft/server"
- // double_jump_scoreboard do player = 2
+import { world, system, InputButton, ButtonState } from "@minecraft/server"
+// double_jump_scoreboard do player = 2
 
-world.afterEvents.playerButtonInput.subscribe((ev) =>{
+world.afterEvents.playerButtonInput.subscribe((ev) => {
     const double_jump_scoreboard = world.scoreboard.getObjective("double_jump")
-    const {player, button, newButtonState} = ev 
+    const { player, button, newButtonState } = ev
     const dash_scoreboard = world.scoreboard.getObjective("dash")
     const dash_score = dash_scoreboard.getScore(player) ?? 0
     const double_jump_score = double_jump_scoreboard.getScore(player) ?? 0
-
-
     const ViewDirection = player.getViewDirection()
-    const vetor = { x: ViewDirection.x * 1.6, z: ViewDirection.z * 1.6};
-    
+    const vetor = { x: ViewDirection.x * 1.6, z: ViewDirection.z * 1.6 };
 
     //verificação se o player apertou o botão de jump
     if (button === InputButton.Jump && newButtonState === ButtonState.Pressed && !player.isOnGround) {
@@ -20,25 +17,24 @@ world.afterEvents.playerButtonInput.subscribe((ev) =>{
         }
         if (button === InputButton.Jump && newButtonState === ButtonState.Pressed && double_jump_score === 0 && dash_score > 0) {
             //funçao de dash
-            player.applyKnockback( vetor,0.5) 
+            player.applyKnockback(vetor, 0.5)
             dash_scoreboard.addScore(player, -1)
-        }  
+        }
     }
-    })
-
-system.runInterval(()=>{
+})
+system.run(() => {
     const dash_max_scoreboard = world.scoreboard.getObjective("dash_max")
     const double_jump_scoreboard = world.scoreboard.getObjective("double_jump")
     const dash_scoreboard = world.scoreboard.getObjective("dash")
 
     for (const player of world.getAllPlayers()) {
         const dash_max_score = dash_max_scoreboard.getScore(player) ?? 0
-        if(player.isOnGround) {
+        if (player.isOnGround) {
 
             dash_scoreboard.setScore(player, dash_max_score)
             double_jump_scoreboard.setScore(player, 1)
-            
-    }
 
-}
- });
+        }
+
+    }
+});
--- development_behavior_packs/cgl_config(bp)/scripts/animal_status.js
+++ development_behavior_packs/cgl_config(bp)/scripts/animal_status.js
@@ -1,7 +1,6 @@
 import { world, system } from "@minecraft/server"
 
-system.runInterval(() =>{
-
+system.run(() =>{
     for (const animal of world.getDimension("overworld").getEntities()){
         const vida_score = animal.getComponent("health")
         if(vida_score) {
