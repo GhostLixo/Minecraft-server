59095644b9d9ada844fe3ea0fc63af53892c7cf9
Arnimisal <arnimisal412@gmail.com>
2025-12-21 21:39:20 -0300

    Update abilidades.js

--- development_behavior_packs/cgl_config(bp)/scripts/abilidades.js
+++ development_behavior_packs/cgl_config(bp)/scripts/abilidades.js
@@ -14,13 +14,13 @@ world.afterEvents.playerButtonInput.subscribe((ev) =>{
     })
 
 system.runInterval(()=>{
-    const ViewDirection = player.getViewDirection()
-    if (dash_score == 0) {
-        player.applyKnockback(ViewDirection, ViewDirection.y)
-    }
      const dash_max_scoreboard = world.scoreboard.getObjective("dash_max")
      const dash_scoreboard = world.scoreboard.getObjective("dash")
     for (const player of world.getAllPlayers()) {
+        const ViewDirection = player.getViewDirection()
+    if (dash_score == 0) {
+        player.applyKnockback(ViewDirection, ViewDirection.y)
+    }
         const dash_max_score = dash_max_scoreboard.getScore(player) ?? 0
         if(player.isOnGround) {
             dash_scoreboard.setScore(player, dash_max_score)
