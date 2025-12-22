7121270460087079c7413ab6d51cdd2d2cbb47a5
Arnimisal <arnimisal412@gmail.com>
2025-12-21 22:01:48 -0300

    Update abilidades.js

--- development_behavior_packs/cgl_config(bp)/scripts/abilidades.js
+++ development_behavior_packs/cgl_config(bp)/scripts/abilidades.js
@@ -5,9 +5,14 @@
 world.afterEvents.playerButtonInput.subscribe((ev) =>{
     const dash_scoreboard = world.scoreboard.getObjective("dash")
     const double_jump_scoreboard = world.scoreboard.getObjective("double_jump")
+    const double_jump_score = double_jump_scoreboard.getScore(player) ?? 0
+    const ViewDirection = player.getViewDirection()
     const {player, button, newButtonState} = ev
     const dash_score = dash_scoreboard.getScore(player) ?? 0
     if (button === InputButton.Jump && newButtonState === ButtonState.Pressed && !player.isOnGround) {
+        if (double_jump_score == 0 && dash_score > 0) {
+        player.applyKnockback(ViewDirection, ViewDirection.y)
+    }
         if(dash_score > 0) {
             double_jump_scoreboard.addScore(player, -1)
         }
@@ -15,16 +20,10 @@ world.afterEvents.playerButtonInput.subscribe((ev) =>{
     })
 
 system.runInterval(()=>{
+    const dash_scoreboard = world.scoreboard.getObjective("dash")
+    const dash_score = dash_scoreboard.getScore(player) ?? 0
      const dash_max_scoreboard = world.scoreboard.getObjective("dash_max")
-     const dash_scoreboard = world.scoreboard.getObjective("dash")
-     const double_jump_scoreboard = world.scoreboard.getObjective("double_jump")
     for (const player of world.getAllPlayers()) {
-        const dash_score = dash_scoreboard.getScore(player) ?? 0
-        const double_jump_score = double_jump_scoreboard.getScore(player) ?? 0
-        const ViewDirection = player.getViewDirection()
-    if (double_jump_score == 0 && dash_score > 0) {
-        player.applyKnockback(ViewDirection, ViewDirection.y)
-    }
         const dash_max_score = dash_max_scoreboard.getScore(player) ?? 0
         if(player.isOnGround) {
             dash_scoreboard.setScore(player, dash_max_score)
