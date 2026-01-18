be4cac35b813544a97acd123099fb821161ca863
Ghost Lixo <martinsclaudiojunior@gmail.com>
2026-01-17 21:22:46 -0300

    sei la

--- development_behavior_packs/cgl_config(bp)/scripts/saudacao.js
+++ development_behavior_packs/cgl_config(bp)/scripts/saudacao.js
@@ -1,14 +1,21 @@
 import { world, system, TicksPerSecond } from "@minecraft/server";
+import { statusSistema } from "./status.js";
+
+let descansarRelogio = null; // Variavel para encerrar o loop do relogio quando não tiver ninguem online
+let descansarStatus = null; // Variavel para encerrar o loop do status quando não tiver ninguem online
+
 
 world.afterEvents.playerJoin.subscribe((ev) => {
     system.runTimeout(() => {
         const mana_scoreboard = world.scoreboard.getObjective("mana");
         const estamina_scoreboard = world.scoreboard.getObjective("estamina");
-        const jugador = world.getAllPlayers().filter((player) => player.id === ev.playerId); // Retorna apenas uma posição de array do objeto player
+        world.getAllPlayers().forEach((player) => {
+            player.onScreenDisplay.setTitle("§aSeja bem-vindo!\n");
+            mana_scoreboard?.setScore(player, 0);
+            estamina_scoreboard?.setScore(player, 0);
+            world.sendMessage("§aSeja bem-vindo ao servidor!\n");}); // Retorna apenas uma posição de array do objeto player
 
-        jugador[0].onScreenDisplay.setTitle("§aSeja bem-vindo!\n");
-        mana_scoreboard?.setScore(jugador[0], 0);
-        estamina_scoreboard?.setScore(jugador[0], 0);
+
     //               codigo legado
     //   for (let keyl of world.getAllPlayers()) {
     //     if (keyl.id === jugador) {
@@ -18,5 +25,30 @@ world.afterEvents.playerJoin.subscribe((ev) => {
     //         }
     //     }
     },TicksPerSecond * 7);
+
+
+//                        Relogio que mostra a hora no chat
+     descansarRelogio = system.runInterval(() => {
+     const relogio = new Date(),
+     variavelMinuto = relogio.getMinutes().toString().padStart(2, "0"),
+     VariavelHora = relogio.getHours() -3,
+     horaFormatada = `${VariavelHora.toString().padStart(2, "0")}:${variavelMinuto}`;
+     world.sendMessage("Hora " + horaFormatada);
+     console.log("Hora " + horaFormatada);
+}, 950);
+//                        Sistema de status importada
+     descansarStatus = system.runInterval(() => {
+     statusSistema();
+});
+
+});
+world.afterEvents.playerLeave.subscribe(() => {
+    if (world.getAllPlayers().length == 0 ) {
+        system.clearRun(descansarRelogio);
+        system.clearRun(descansarStatus);
+        descansarRelogio = null;
+        descansarStatus = null;
+        console.log("Loop do relogio parou.");
+    }
 });
 
--- development_behavior_packs/cgl_config(bp)/scripts/status.js
+++ development_behavior_packs/cgl_config(bp)/scripts/status.js
@@ -1,37 +1,32 @@
 import { world, system} from "@minecraft/server"
-
-system.runInterval(() =>{
-     const mana_scoreboard = world.scoreboard.getObjective("mana")
-     const estamina_scoreboard = world.scoreboard.getObjective("estamina")
+export function statusSistema() {
+     const mana_scoreboard = world.scoreboard.getObjective("mana");
+     const estamina_scoreboard = world.scoreboard.getObjective("estamina");
     //sistema de interfaca do status
     for (const jogador of world.getAllPlayers()) {
-        const stm_score = estamina_scoreboard.getScore(jogador) ?? 0
-        const mn_score = mana_scoreboard.getScore(jogador) ?? 0
+        const stm_score = estamina_scoreboard.getScore(jogador) ?? 0;
+        const mn_score = mana_scoreboard.getScore(jogador) ?? 0;
          if (jogador.isSprinting ){
             if(stm_score > 0) { //stm_score é a estamina
-                estamina_scoreboard.addScore(jogador, -1)
+                estamina_scoreboard.addScore(jogador, -1);
             }   
         }
         else if (!jogador.isSprinting) {
              if (stm_score < 200) {
-                estamina_scoreboard.addScore(jogador, 1)
+                estamina_scoreboard.addScore(jogador, 1);
             }
         }
         if (stm_score == 0) {
-            jogador.inputPermissions.setPermissionCategory(2, false)
+            jogador.inputPermissions.setPermissionCategory(2, false);
         } 
         if (stm_score == 10) {
-            jogador.inputPermissions.setPermissionCategory(2, true)    
+            jogador.inputPermissions.setPermissionCategory(2, true); 
         }
 
-        const vida_score = jogador.getComponent("health")
+        const vida_score = jogador.getComponent("health");
         jogador.onScreenDisplay.setActionBar(` ${vida_score.currentValue.toFixed(1)}\nmana ${mn_score}\n${stm_score}
-            `)
+            `);
     }
-});
-system.runInterval(() => {
-    let agora = new Date(); let minuto = agora.getMinutes().toString().padStart(2, "0"); let hora = agora.getHours() -3;
-    let horaFormatada = `${hora.toString().padStart(2, "0")}:${minuto}`;
-    world.sendMessage("Hora " + horaFormatada);
-    console.log("Hora " + horaFormatada);
-}, 950);
+    console.log("Sistema de status rodando...");
+
+}
